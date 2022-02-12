3abe0258076703b2fc4cade80232db4e9334ced5
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-02-12 16:18:48 +0545

    chores(deps): updated deps

--- vite.config.ts
+++ vite.config.ts
@@ -50,7 +50,7 @@ export default defineConfig(({ mode }) => {
       shortcuts: [
         [
           'btn',
-          'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer no-underline hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50',
+          'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer no-underline disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50 hover:bg-red-200',
         ],
         [
           'icon-btn',
