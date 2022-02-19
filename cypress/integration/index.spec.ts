77ac0b16b3e87f8ecec2ad888c3a684a4cc8f4bc
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-02-19 22:50:29 +0545

    test(cypress): added tests

---
+++ cypress/integration/index.spec.ts
@@ -0,0 +1,39 @@
+context('Basic', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('basic nav', () => {
+    cy.url().should('eq', 'http://localhost:4000/');
+    cy.contains('h1', 'Hello 👋 there, welcome to vitely');
+  });
+
+  it('should go to links', () => {
+    const pages = [
+      {
+        name: 'VSCode',
+        url: 'https://code.visualstudio.com/',
+      },
+      {
+        name: 'Volar',
+        url: 'https://github.com/johnsoncodehk/volar',
+      },
+      {
+        name: 'Vetur',
+        url: 'https://marketplace.visualstudio.com/items?itemName=octref.vetur',
+      },
+      {
+        name: 'Vite Docs',
+        url: 'https://vitejs.dev/guide/features.html',
+      },
+      {
+        name: 'Rubiin',
+        url: 'https://github.com/rubiin',
+      },
+    ];
+
+    pages.forEach(page => {
+      cy.contains(page.name).should('have.prop', 'href', page.url);
+    });
+  });
+});
